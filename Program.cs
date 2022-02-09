e6abc02953059bc4843ba10542c51fb5db549a13
cstazinski29 <cstazinski@gmail.com>
2022-02-09 14:42:55 -0500

    created outline of coding steps and README

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Nerdle!");
+﻿// Randomly generate 3 digit number
+    // random
+// Take in user input
+    // Console.readline
+// Compare digits
+    // using FOREACH() 
+// Provide feedback
+    // if statements
+// Repeat for 6 turns (if needed)
+    // use a loop?
+// Display end result
+
+//int randomNerdle = 
